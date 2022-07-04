0ba04db3d864e8f01adfc61f2125d0f3c816d8d6
PraYash42 <96410587+prayash42@users.noreply.github.com>
2022-07-05 02:03:11 +0530

    Update dsa trial.c

--- giit/dsa trial.c
+++ giit/dsa trial.c
@@ -1,62 +1,64 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
 struct node{
     int data;
     struct node * next;
-}*head,*tail,*new,*temp;
+}*head,*tail,*temp,*new;
 int main(){
-    int value;
-    int a=1;
+    int value,a=1;
     do{
+        
+    
         new=(struct node*)malloc(sizeof(struct node));
-        printf("enter data\n");
+        printf("enter data");
         scanf("%d",&value);
         new->data=value;
         new->next=NULL;
         if(head==NULL){
             head=new;
             tail=new;
-        }
+    }
         else{
             tail->next=new;
             tail=new;
+            tail->next=head;
         }
-        printf("1 to cont \n 0 to break");
-        //char ch;
+        printf("1 to continue\n0 to stop");
         scanf("%d",&a);
-
     }while(a==1);
-    
-printf("inswrttion\n");
-new=(struct node *)malloc(sizeof(struct node));
-printf("enter value");
-scanf("%d",&new->data);
-new->next=head;
-head=new;
-printf("insertion at end");
-new=(struct node *)malloc(sizeof(struct node));
-printf("enter value");
-scanf("%d",&new->data);
-tail->next=new;
-new->next=NULL;
-tail=new;
-printf("enter position\n");
-int pos;
-scanf("%d",&pos);
+
 temp=head;
-new=(struct node *)malloc(sizeof(struct node));
-printf("enter value");
-scanf("%d",&new->data);
-for(int i=0;i<pos-1;i++){
+while(temp->next!=head){
+    printf("%d\n",temp->data);
     temp=temp->next;
 }
-    new->next=temp->next;
-    temp->next=new;
+printf("%d \n",temp->data);
+printf("at first insertion\n");
+ new=(struct node*)malloc(sizeof(struct node));
+ printf("enter data\n");
+ scanf("%d",&value);
+ new->data=value;
+ new->next=head;
+ tail->next=new;
+ head=new;
+ printf("deleting from starting\n");
+ temp=head;
+ tail->next=temp->next;
+ head=head->next;
+ temp->next=NULL;
+ printf("deleting from end\n");
+ temp=head;
+ while(temp->next!=tail){
+     temp=temp->next;
+ }
+ temp->next=head;
+ tail->next=NULL;
+ tail=temp;
+ temp=head;
+ while(temp->next!=head){
+    printf("%d",temp->data);
+    temp=temp->next;
+}
+printf("%d",temp->data);
 
-
-temp=head;
-    while(temp!=NULL){
-        printf("node is%d\n",temp->data);
-        temp=temp->next;
-    }
 }
